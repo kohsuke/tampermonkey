bc48bf68143f6eb568e9f4b549d9c12301648329
Kohsuke Kawaguchi <kk@kohsuke.org>
2025-03-03 08:45:20 -0800

    Initial import

---
+++ confluence-banner-killer.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Confluence banner killer
+// @namespace    https://kohsuke.org/
+// @version      2025-02-05
+// @description  try to take over the world!
+// @author       You
+// @match        https://cloudbees.atlassian.net/wiki/spaces/*/pages/edit-v2/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function() {
+        document.getElementById("AkTopNav").style.display = "none";
+        document.querySelector("DIV#confluence-ui DIV[data-layout-container]").style.gridTemplateRows = "0px 0px auto";
+    }, 1000);
+})();
